28b8de540a90ac30edc70f781b045da79dbe51e1
Mika <mika@rootroo.com>
2021-02-23 21:02:02 +0200

    with progress

--- corpona/summary.py
+++ corpona/summary.py
@@ -1,21 +1,23 @@
 from collections import defaultdict
-
+from tqdm import tqdm
 
 def strtype(v): return str(type(v).__name__)
 
 
-def summarize(data):
+def summarize(data, show_progress=True):
     summary = defaultdict(list)
-
+    _t = tqdm
+    if show_progress is False:
+        _t = list
     if isinstance(data, dict):
-        for k, v in data.items():
-            _v = summarize(v)
+        for k, v in _t(data.items()):
+            _v = summarize(v, show_progress=False)
             summary[k] = _v
     elif isinstance(data, str):
         return [strtype(data), ]
     elif isinstance(data, list):
-        for v in data:
-            _v = summarize(v)
+        for v in _t(data):
+            _v = summarize(v, show_progress=False)
             for __v in _v:
                 if __v not in summary[strtype(v)]:
                     summary[strtype(v)].append(__v)
